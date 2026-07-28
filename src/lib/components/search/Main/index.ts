86652601a6d4856330125f4f37949b779ff60675
unknown <alptangal@gmail.com>
2026-07-28 18:17:25 +0700

    .

--- src/lib/components/search/Main/index.ts
+++ src/lib/components/search/Main/index.ts
@@ -1,11 +1,8 @@
 import SearchMainControl from './Control/Main.svelte';
 import SearchMainIcon from '$components/element/icon/Main.svelte';
 import SearchMainRoot from './Root/Main.svelte';
-import SearchMainInput from './Input/Main.svelte'
-
 
 export const SearchMain = Object.assign(SearchMainRoot, {
-  Control: SearchMainControl,
-  Icon: SearchMainIcon,
-  Input: SearchMainInput
-})
+	Control: SearchMainControl,
+	Icon: SearchMainIcon
+});
