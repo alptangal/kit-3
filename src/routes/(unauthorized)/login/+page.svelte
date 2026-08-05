428265b59ca3197b9712a086acea28d8cf259e2e
unknown <alptangal@gmail.com>
2026-08-05 08:28:35 +0700

    .

--- src/routes/(unauthorized)/login/+page.svelte
+++ src/routes/(unauthorized)/login/+page.svelte
@@ -26,7 +26,7 @@
 				onClick={async () => {
 					const publicKeyOptions = {
 						challenge: new Uint8Array(32), // random, từ server
-						rp: { name: 'MyApp', id: 'https://kit-3.vercel.com' },
+						rp: { name: 'MyApp' },
 						user: {
 							id: new TextEncoder().encode('userId'),
 							name: 'user@example.com',
